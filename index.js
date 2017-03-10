6be32f1d2b4b63f26f8ece50714ff3a4a9b94407
xingoxu <xingoxu@gmail.com>
2017-03-10 09:18:37 +0800

    change some lines' indent

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 function twoWay(object,dom){
-  if(!object.__cb__) {
-    Object.defineProperty(object,'__cb__',{value: [], enumerable: false})
-  }
+  if(!object.__cb__) Object.defineProperty(object,'__cb__',{value: [], enumerable: false})
+  
   let objectValue = undefined;
   if(!object.hasOwnProperty('value')){
     Object.defineProperty(object,'value',{
       'get'(){ return objectValue; },
       'set'(newVal){
         if(object.value == newVal) return;
-        object.__cb__.forEach(cb=>{
-          cb(newVal);
-        })
+        object.__cb__.forEach( cb =>{ cb(newVal); })
         objectValue = newVal;
       }
     })
   }
-  object.__cb__.push(value=>{
+  object.__cb__.push( value => {
     let type = {
       'checkbox'(){ dom.checked = !!value; },
       'radio'(){ dom.checked = (value == dom.value) },
       'default'(){ dom.value = value; }
     }
     type[dom.type] ? type[dom.type]() : type.default();
-   });
-  let eventCallBack = ()=>{ 
+  });
+  let eventCallBack = () => {
     let type = {
       'checkbox'(){	object.value = dom.checked; },
       'radio'(){ dom.checked ? object.value = dom.value : true ; },
